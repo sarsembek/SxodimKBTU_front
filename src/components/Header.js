968ef05715d04fdfa8254f36ca0d8c8e764e4d1c
Arman <ar_sarsembek@kbtu.kz>
2023-10-09 20:25:50 +0600

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+// src/components/Header.js
+import React from 'react';
+import { Layout} from 'antd';
+import logo from 'C:\\Users\\777\\Desktop\\TorrentProject\\kztracker\\src\\radar.png';
+import './Header.css'
+
+const { Header } = Layout;
+
+export const AppHeader = () => {
+  return (
+    <Header
+      style={{
+        position: 'sticky',
+        top: 0,
+        zIndex: 1,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+        <div className='logo'>
+            <img id='logo' src={logo} alt='logo'/>
+        </div>
+    </Header>
+  );
+};
+
+export default AppHeader;
