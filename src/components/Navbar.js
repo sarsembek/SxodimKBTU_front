a545f90ee97bcf9bb02a91f542bd6d47fa7a52a2
Arman <ar_sarsembek@kbtu.kz>
2023-10-10 19:36:58 +0600

    navbar started

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { HomeFilled, DownloadOutlined, CheckCircleOutlined, MailOutlined, TeamOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import './Navbar.css'
+
+const items = [
+    {
+        label: 'Home',
+        key: 'home',
+        icon: <HomeFilled />
+    },
+    {
+        label: 'Upload',
+        key: 'upload',
+        icon: <DownloadOutlined />
+    },
+    {
+        label: 'Rules',
+        key: 'rules',
+        icon: <CheckCircleOutlined />
+    },
+    {
+        label: 'Contact',
+        key: 'contact',
+        icon: <MailOutlined />
+    },
+    {
+        label: 'About Us',
+        key: 'about_us',
+        icon: <TeamOutlined />
+    }
+]
+
+export const Navbar = () => {
+    const [current, setCurrent] = useState('home');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+    return (
+        <Menu 
+            id='navbar' 
+            onClick={onClick} 
+            selectedKeys={[current]} 
+            mode="horizontal" 
+            items={items} 
+        />
+    )
+}
